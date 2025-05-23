a75e1cbc4eb1bae00c1aa25bc55866141b1864ec
adri <adri.holzer.e@gmail.com>
2025-05-23 01:33:03 -0300

    url produccion

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,1 +1,2 @@
-export const API_BASE_URL: string = 'http://localhost:8080';
+//export const API_BASE_URL: string = 'http://localhost:8080';
+export const API_BASE_URL: string = 'https://vps-4291415-x.dattaweb.com';
